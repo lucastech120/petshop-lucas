c69ae6841a15b79b7aa8d4ec302a208ba62f8a74
lucastech120 <lucastech120@gmail.com>
2025-10-16 21:55:28 -0300

    Fix:Github-repo+ cripts/pages+Actualizarcarrito

--- js/utils.js
+++ js/utils.js
@@ -1,8 +1,16 @@
 export function actualizarContadorCarrito() {
   const contador = document.getElementById("contador-carrito");
-  if (!contador) return; // si el elemento no existe, salimos
+  if (!contador) return;
 
   const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
   const totalCantidad = carrito.reduce((acc, prod) => acc + prod.cantidad, 0);
   contador.textContent = totalCantidad;
+
+  if (totalCantidad > 0) {
+    contador.style.display = "inline-block";
+  } else {
+    contador.style.display = "inline-block";
+  }
 }
+
+document.addEventListener("DOMContentLoaded", actualizarContadorCarrito);
